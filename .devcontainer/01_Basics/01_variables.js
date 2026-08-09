9bb49ca8a15c3ebe74c1bdc168be24d9629e35d8
amarnathwbjee <amarnathwbjee@gmail.com>
2026-08-09 13:01:56 +0000

    conversion of datatypes

---
+++ .devcontainer/01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "amarnath@google.com"
+var accountPassword = "12345"
+accountCity = "Sitamarhi"
+let accountState;
+
+
+console.log(accountId); 
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+prefer not to use var because of issue in block scope and functional scope
+
+*/
---
+++ .devcontainer/01_Basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS Code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3) // code readability should be high
+console.log("Amarnath")
+
+let name = "Amarnath Kumar"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object 
+
+console.log(typeof age); // number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
