257a044f9535c6273a569ad921ab77e47b9bfb8e
amarnathwbjee <amarnathwbjee@gmail.com>
2026-08-15 05:07:57 +0000

    07_nums_and_math.js

---
+++ .devcontainer/01_Basics/07_nums_and_math.js
@@ -0,0 +1,33 @@
+const Score = 400
+// console.log(Score);
+
+const balance = new Number (100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(5, 57, 34, 67, 89));
+// console.log(Math.max(5, 57, 34, 67, 89));
+
+// console.log(Math.random()); #value between 0 and 1
+// console.log(Math.random()*10);
+// console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
