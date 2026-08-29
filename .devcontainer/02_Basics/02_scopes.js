1967104ed5f4936f44c9d4acabd11b840d1e1ad8
amarnathwbjee <amarnathwbjee@gmail.com>
2026-08-29 09:58:08 +0000

    02_scopes

--- .devcontainer/02_Basics/02_scopes.js
+++ .devcontainer/02_Basics/02_scopes.js
@@ -1,12 +1,63 @@
+// var c = 300
+let a = 300
 if(true) {
     let a = 10
     const b = 20
-    var c = 30
+     c = 30
+    // console.log("INNER: ", a); 
 
 
 }
 
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+
+
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+function one() {
+    const username = "Amarnath"
+
+    function two() {
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+
+    two()
+
+}
+
+one()
+
+if (true) {
+    const username = "Amarnath"
+    if (username === "Amarnath") {
+        const website = " youtube"
+    //     console.log(username + website);
+    }
+//     console.log(website);
+}
+
+// console.log(username);
+
+// ++++++++++++++ interesting +++++++++++++
+
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+
+
+
+console.log(addTwo()5)
+const addTwo = function(num){
+    return num + 2
+}
+
