fd2959bbcbfe3b5f1666fb9f65bf15cab6132a0b
amarnathwbjee <amarnathwbjee@gmail.com>
2026-09-05 21:34:08 +0000

    04_four.js

---
+++ .devcontainer/05_iterations/04_four.js
@@ -0,0 +1,27 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+    
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming)  {
+    // console.log(key);
+    // console.log(programming[key]);
+    
+}
+
+for (const key in object) {
+    console.log(key);
+    
+    
+} 
+}
