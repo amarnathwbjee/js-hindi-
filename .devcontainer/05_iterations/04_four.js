3dd3765ecfb7a3ccef9347162e605e8945187d9f
amarnathwbjee <amarnathwbjee@gmail.com>
2026-09-06 17:28:29 +0000

    05_five

--- .devcontainer/05_iterations/04_four.js
+++ .devcontainer/05_iterations/04_four.js
@@ -23,5 +23,16 @@ for (const key in object) {
     console.log(key);
     
     
-} 
 }
+
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
